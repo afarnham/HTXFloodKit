e77fceae3ad159222aeb8d34e2186e527b74e8d0
Aaron Farnham <aaron@foreflight.com>
2017-09-06 22:37:52 -0500

    Initial Commit

---
+++ HTXFloodKit/HTXFloodKit.h
@@ -0,0 +1,19 @@
+//
+//  HTXFloodKit.h
+//  HTXFloodKit
+//
+//  Created by Raymond Farnham on 9/6/17.
+//  Copyright © 2017 TinyRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTXFloodKit.
+FOUNDATION_EXPORT double HTXFloodKitVersionNumber;
+
+//! Project version string for HTXFloodKit.
+FOUNDATION_EXPORT const unsigned char HTXFloodKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTXFloodKit/PublicHeader.h>
+
+
